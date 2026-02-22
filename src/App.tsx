418339bc33842ed8a23095c151d534b17a98798a
mathantunes <matheus.antunesdejesus@valora.com>
2026-02-22 18:54:58 +0100

    Fix dev env navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import PageWrapper from './components/PageWrapper';
 import AppRoutes from './routes';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
+  const isProduction = import.meta.env.MODE === 'production';
+  const Router = isProduction ? HashRouter : BrowserRouter;
+
   return (
     <ThemeProvider>
-      <HashRouter>
+      <Router>
         <PageWrapper>
           <AppRoutes />
         </PageWrapper>
-      </HashRouter>
+      </Router>
     </ThemeProvider>
   );
 }
